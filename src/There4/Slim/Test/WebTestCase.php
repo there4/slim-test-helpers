8ceccb7a307e92f3ab92bd018e8981248789c69b
guillermoandrae <guillermoandraefisher@gmail.com>
2014-10-21 15:26:35 -0400

    Made the class PSR-2 compliant and added the property names so IDEs don't freak out.

--- src/There4/Slim/Test/WebTestCase.php
+++ src/There4/Slim/Test/WebTestCase.php
@@ -2,8 +2,13 @@
 
 namespace There4\Slim\Test;
 
+use \Slim\Slim;
+
 class WebTestCase extends \PHPUnit_Framework_TestCase
 {
+    protected $app;
+    protected $client;
+
     // Run for each unit test to setup our slim app environment
     public function setup()
     {
@@ -14,13 +19,12 @@ class WebTestCase extends \PHPUnit_Framework_TestCase
 
     // Instantiate a Slim application for use in our testing environment. You
     // will most likely override this for your own application.
-    public function getSlimInstance() {
-        return new \Slim\Slim(array(
+    public function getSlimInstance()
+    {
+        return new Slim(array(
             'version' => '0.0.0',
             'debug'   => false,
             'mode'    => 'testing'
         ));
     }
 }
-
-/* End of file WebTestCase.php */
